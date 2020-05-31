6183e2b290641278e5f519e2a5e6779ff94aac87
Jon Lim <@jlim1010>
2020-05-31 16:47:34 -0700

    add merging and quitting

--- Program.fs
+++ Program.fs
@@ -7,81 +7,94 @@ open SpacedRepetition.Net
 open SpacedRepetition.Net.ReviewStrategies
 
 type vocab = { French : string; English : string }
-type vocabReviewItem = ReviewItem<vocab>
+
+type UserAction = ReviewOutcome of ReviewOutcome | Quit
 
 let getNow() = DateTime.Now
 
 let help() =
-    printfn "Hit enter to get translation. Outcome: 1 Perfect 2 Hesitant 3 Incorrect. ? to display this"
+    printfn "Hit enter to get translation. Outcome: 0 Stop 1 Perfect 2 Hesitant 3 Incorrect. ? to display this"
 
-let quiz (session : StudySession<vocab>) (item : vocabReviewItem) : vocabReviewItem =
-    let rec getInput() : ReviewOutcome Option =
+let quizzer (session : StudySession<vocab>) (quizResults : ReviewItem<vocab> List) : 
+    (ReviewItem<vocab> * ReviewItem<vocab> List) option =
+    let rec getInput() : UserAction Option =
         let userOutcome = Console.ReadKey().KeyChar
         printfn ""
-        match userOutcome with 
-        | '1' -> Some ReviewOutcome.Perfect
-        | '2' -> Some ReviewOutcome.Hesitant
-        | '3' -> Some ReviewOutcome.Incorrect
+        match userOutcome with
+        | '0' -> Some Quit
+        | '1' -> Some (ReviewOutcome ReviewOutcome.Perfect)
+        | '2' -> Some (ReviewOutcome ReviewOutcome.Hesitant)
+        | '3' -> Some (ReviewOutcome ReviewOutcome.Incorrect)
         | '?' -> help()
                  getInput()
         | _ ->   None
 
-    let rec needInput() : ReviewOutcome =
+    let rec needInput() : UserAction =
         match getInput() with
         | Some outcome -> outcome
         | None -> needInput()
 
-    Console.Clear();
-    printfn "français: %s" item.Item.French
+    match Seq.tryHead session with
+    | None -> None
+    | Some item ->    
+        Console.Clear();
+        printfn "français: %s" item.Item.French
 
-    let outcomeEarly = getInput()
+        let outcomeEarly = getInput()
 
-    printfn "anglais: %s" item.Item.English
-    printfn ""
+        printfn "anglais: %s" item.Item.English
+        printfn ""
     
-    let result = 
-        match outcomeEarly with
-        | Some outcome -> outcome
-        | None -> 
-            needInput()
+        let action = 
+            match outcomeEarly with
+            | Some outcome -> outcome
+            | None -> needInput()
     
-    session.Review(item, result)
+        match action with
+        | Quit -> None
+        | ReviewOutcome result -> 
+            let reviewedItem = session.Review(item, result)
+            Some (reviewedItem, reviewedItem::quizResults)
 
-let AskVocabList() =
-    let defaultData = [
-        {
-            Status = NeverReviewed
-            Item = { French = "jamais"; English = "never" }
-        }
-        {
-            Status = NeverReviewed
-            Item = { French = "peut-être"; English = "maybe" } 
-        }
-    ]
+let rec AskVocabList() =
     printfn "Enter filename of json or enter for default: "
-    let line = Console.ReadLine()
-    if (File.Exists(line)) then
+    let line = Console.ReadLine().Trim()
+    let line = if line = "" then "vocab.txt" else line
+    if not (File.Exists(line)) then
+        printfn "error finding file. restarting..."
+        AskVocabList()
+    else
         let text = File.ReadAllText(line)
         match (Decode.Auto.fromString text) with
         | Ok data ->
             printfn "loaded %s" line
             data
         | _ -> 
-            defaultData
-    else
-        defaultData
+            printfn "error loading data. restarting..."
+            AskVocabList()
+
+let merge (primary : ReviewItem<vocab> List) (secondary : ReviewItem<vocab> List) =
+    let extractVocab (i : ReviewItem<vocab>) = i.Item
+    let addIfNotPresent state (item : ReviewItem<vocab>) =
+        if (List.map extractVocab state) |> List.contains item.Item then
+            state
+        else
+            item::state
+    List.fold addIfNotPresent [] (primary@secondary)
 
 [<EntryPoint>]
 let main argv =
-    let vocabSet : vocabReviewItem List  = AskVocabList()
+    let vocabSet : ReviewItem<vocab> List  = AskVocabList()
     let strategy = new SuperMemo2ReviewStrategy(getNow)
     let studySession = new StudySession<vocab>(vocabSet, getNow, strategy, 3, 3)
-    let mapWithQuiz = quiz studySession
+    let stepQuiz = quizzer  studySession
     help()
     printfn "Press enter to start..."
     Console.ReadKey() |> ignore;
-    let result = Seq.map mapWithQuiz studySession
-    let json = Encode.Auto.toString(4, Seq.toList(result))
+    let sessionResult = Seq.toList (Seq.unfold stepQuiz [])
+    // merge result with original list, remove originals if a match...
+    let result : ReviewItem<vocab> List = (merge sessionResult vocabSet)
+    let json = Encode.Auto.toString(4, result)
     printfn "Enter to save to disk or ctrl-c exit..."
     Console.ReadLine() |> ignore
     File.WriteAllText("vocab.txt", json)
