850555a999de27fa77b87c9465f803c22f22e926
Jon Lim <@jlim1010>
2020-05-31 14:38:55 -0700

    add file io

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
 open Thoth.Json.Net
 open SpacedRepetition.Net
 open SpacedRepetition.Net.ReviewStrategies
@@ -46,9 +47,8 @@ let quiz (session : StudySession<vocab>) (item : vocabReviewItem) : vocabReviewI
     
     session.Review(item, result)
 
-[<EntryPoint>]
-let main argv =
-    let vocabSet : vocabReviewItem List  = [
+let AskVocabList() =
+    let defaultData = [
         {
             Status = NeverReviewed
             Item = { French = "jamais"; English = "never" }
@@ -57,7 +57,23 @@ let main argv =
             Status = NeverReviewed
             Item = { French = "peut-être"; English = "maybe" } 
         }
-        ]
+    ]
+    printfn "Enter filename of json or enter for default: "
+    let line = Console.ReadLine()
+    if (File.Exists(line)) then
+        let text = File.ReadAllText(line)
+        match (Decode.Auto.fromString text) with
+        | Ok data ->
+            printfn "loaded %s" line
+            data
+        | _ -> 
+            defaultData
+    else
+        defaultData
+
+[<EntryPoint>]
+let main argv =
+    let vocabSet : vocabReviewItem List  = AskVocabList()
     let strategy = new SuperMemo2ReviewStrategy(getNow)
     let studySession = new StudySession<vocab>(vocabSet, getNow, strategy, 3, 3)
     let mapWithQuiz = quiz studySession
@@ -66,5 +82,7 @@ let main argv =
     Console.ReadKey() |> ignore;
     let result = Seq.map mapWithQuiz studySession
     let json = Encode.Auto.toString(4, Seq.toList(result))
-    printfn "json: %s" json
+    printfn "Enter to save to disk or ctrl-c exit..."
+    Console.ReadLine() |> ignore
+    File.WriteAllText("vocab.txt", json)
     0 // return an ok exit code
