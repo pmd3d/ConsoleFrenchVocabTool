80c43285bedb7a0e44c4159bd2ba9a4bde5712cc
Jon Lim <@jlim1010>
2020-06-14 07:48:41 -0700

    remove inner loop in merge

--- Program.fs
+++ Program.fs
@@ -13,10 +13,11 @@ open SpacedRepetition.Net
 open SpacedRepetition.Net.ReviewStrategies
 
 type vocab = { French : string; English : string }
+type vocabi = { French : string; English : string }
 
 type UserAction = ReviewOutcome of ReviewOutcome | Quit
 
-let getNow() = DateTime.Now
+let getNow() = DateTime.Now.AddDays(180.0)
 
 let help() =
     printfn "Hit enter to get translation. Outcome: 0 Stop 1 Perfect 2 Hesitant 3 Incorrect. ? to display this"
@@ -80,26 +81,26 @@ let rec AskVocabList() =
             AskVocabList()
 
 let merge (primary : ReviewItem<vocab> List) (secondary : ReviewItem<vocab> List) =
-    let extractVocab (i : ReviewItem<vocab>) = i.Item
-    let addIfNotPresent state (item : ReviewItem<vocab>) =
-        if (List.map extractVocab state) |> List.contains item.Item then
-            state
+    let addIfNotPresent (lookup : Set<vocab>, state) (item : ReviewItem<vocab>) =
+        if lookup.Contains item.Item then
+            (lookup, state)
         else
-            item::state
-    List.fold addIfNotPresent [] (primary@secondary)
+            (lookup.Add(item.Item)), item::state
+    let _, result = List.fold addIfNotPresent (Set.empty,[]) (primary@secondary)
+    result
 
 [<EntryPoint>]
 let main argv =
-    let vocabSet : ReviewItem<vocab> List  = AskVocabList()
+    let vocabList : ReviewItem<vocab> List  = AskVocabList()
     let strategy = new SuperMemo2ReviewStrategy(getNow)
-    let studySession = new StudySession<vocab>(vocabSet, getNow, strategy, 3, 3)
-    let stepQuiz = quizzer  studySession
+    let studySession = new StudySession<vocab>(vocabList, getNow, strategy, 3, 3)
+    let stepQuiz = quizzer studySession
     help()
     printfn "Press enter to start..."
     Console.ReadKey() |> ignore;
     let sessionResult = Seq.toList (Seq.unfold stepQuiz [])
     // merge result with original list, remove originals if a match...
-    let result : ReviewItem<vocab> List = (merge sessionResult vocabSet)
+    let result : ReviewItem<vocab> List = (merge sessionResult vocabList)
     let json = Encode.Auto.toString(4, result)
     printfn "Enter to save to disk or ctrl-c exit..."
     Console.ReadLine() |> ignore
